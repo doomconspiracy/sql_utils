75ef12ccdca81888a8fb79697899257584ccb647
Matthew Silvera <matthewsilvera@gmail.com>
2019-03-30 08:49:30 -0700

    move to installable py package

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='sql_utils',
+    version='0.1',
+    description='A collection of python sql and db utilites.',
+    author='Matthew Silvera',
+    packages=['sql_utils'],
+)
